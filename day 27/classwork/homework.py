d9dc26d5874f7c1cd9b65517d7d0f4a4779551d6
BEQAGAVASHELI <beka261011@gmail.com>
2024-08-13 18:26:23 +0200

    first commit

---
+++ day 27/classwork/homework.py
@@ -0,0 +1,27 @@
+#davaleba 1 
+list = (10 , 10 , 25)
+print(list )
+
+#davaleba 2 
+
+#banana =int(input("enter 1 number..."))
+#banana =int(input("enter 1 number..."))
+#banana =int(input("enter 1 number..."))
+#banana =int(input("enter 1 number..."))
+#banana =int(input("enter 1 number..."))
+#banana =int(input("enter 1 number..."))
+
+
+
+#for i in range(banana,50,2)
+#print(i)
+
+
+
+
+
+#for i in range(banana,50,1)
+#print(i)
+
+
+
