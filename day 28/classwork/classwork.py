2f7831e4e7ab87489dd6e6ef8f4ccae0bdb0d44d
BEQAGAVASHELI <beka261011@gmail.com>
2024-08-30 17:18:42 +0200

    second commit

---
+++ day 28/classwork/classwork.py
@@ -0,0 +1,72 @@
+#Module 1 
+
+
+#davaleba 1
+
+
+
+hour = 60 * 60
+print(hour * 24)
+
+
+
+#davaleba 2
+
+
+print("price:87")
+
+
+
+
+
+#davaleba 3
+
+
+print("Nikname" , "Master")
+print("score" , 99)
+
+
+
+
+#davaleba 4
+
+#print  *
+#      ***
+#     ***** 
+#      ***
+#       *
+
+
+
+
+#Module 2 
+
+
+
+#davaleba 1
+
+print("country:Portugal ")
+print("capital:Lisbon ")
+
+
+
+
+#davaleba 2
+
+
+
+
+
+
+
+
+#davaleba 3
+
+
+
+
+
+
+
+
+
